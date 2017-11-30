4c5615c21cac812e1d701189a11f744cff2bfff8
Bob Burns <reburns@protonmail.com>
2017-11-29 16:36:02 -0800

    version char* in main

--- src/main.c
+++ src/main.c
@@ -1,7 +1,20 @@
+/* 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #include "common.h"
 #include <getopt.h>
 
 static int usage_flag;
+char * version = "Mikode 1.0.1";
 
 int usage()
 {
@@ -24,7 +37,11 @@ int print_help()
 }
 int print_version()
 {
-	printf("\n** Mikode -- version 1.0 **\n");
+	printf("\n%s\n",version);
+	printf("Copyright (C) 2017  Bob Burns\n"
+		"License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n"
+		"This is free software: you are free to change and redistribute it.\n"
+		"There is NO WARRANTY, to the extent permitted by law.\n");
 	return 0;
 }
 
